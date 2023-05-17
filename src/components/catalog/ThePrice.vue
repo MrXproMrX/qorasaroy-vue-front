8e74f132b77508f0f4ab53287cac7b46fd467faf
MRXPRO200330020179Y <mrxpro2003@gmail.com>
2023-05-17 14:27:33 +0500

    chore: catalog

--- src/components/catalog/ThePrice.vue
+++ src/components/catalog/ThePrice.vue
@@ -1,22 +1,12 @@
-<script lang="ts" setup>
-import Slider from '@vueform/slider'
-const value = [1000, 6000]
-</script>
 <template>
   <div class="allProducts__categories">
     <div class="allProducts__categories__cart">
       <h3 class="allProducts__categories__title">Цена</h3>
     </div>
-    <Slider
-      class="allProducts__slider"
-      v-model="value"
-      :format="{ thousand: ' ' }"
-      :step="50"
-      :max="10000"
-    />
+    <Slider class="allProducts__slider" v-model="value" hide-details :step="50" :max="10000" />
     <div class="allProducts__price__list">
-      <h3 class="allProducts__price__title">1000</h3>
-      <h3 class="allProducts__price__title">6000</h3>
+      <h3 class="allProducts__price__title">{{ value[0] }}</h3>
+      <h3 class="allProducts__price__title">{{ value[1] }}</h3>
     </div>
 
     <div class="allProducts__price__cart">
@@ -27,3 +17,17 @@ const value = [1000, 6000]
 </template>
 
 <style src="@vueform/slider/themes/default.css"></style>
+
+<script>
+import Slider from '@vueform/slider'
+export default {
+  data() {
+    return {
+      value: [1000, 6000],
+    }
+  },
+  components: {
+    Slider,
+  },
+}
+</script>
